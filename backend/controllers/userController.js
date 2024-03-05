949fea94d6cbe26ef9175d637ad6179b1568e4c6
Jeshnil Kumar <kumar_796@hotmail.com>
2024-03-06 02:31:41 +1100

    added backend implementation of get user profile and update user profile

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -39,12 +39,13 @@ const registerUser = asyncHandler(async (req, res) => {
         throw new Error('User already exists');
     }
 
+    // create user
     const user = await User.create({
         name,
         email,
         password,
     });
-    // create user
+    // if given valid user form details
     if (user) {
         generateToken(res, user._id);
         
@@ -59,10 +60,10 @@ const registerUser = asyncHandler(async (req, res) => {
     }
     });
   
-  // @desc    Logout user / clear cookie
-  // @route   POST /api/users/logout
-  // @access  Public
-  const logoutUser = (req, res) => {
+// @desc    Logout user / clear cookie
+// @route   POST /api/users/logout
+// @access  Public
+const logoutUser = (req, res) => {
     res.cookie('jwt', '', {
       httpOnly: true,
       expires: new Date(0),
@@ -70,8 +71,57 @@ const registerUser = asyncHandler(async (req, res) => {
     res.status(200).json({ message: 'Logged out successfully' });
   };
 
+
+// @desc    Get user details
+// @route   GET /api/users/profile
+// @access  Private
+// since @access is Private, req body contains user._Id from protect method in userRoutes.js
+const getUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    res.json({
+      _id: user._id,
+      name: user.name,
+      email: user.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+})
+
+// @desc  Update user profile
+// @route  PUT /api/users/profile 
+// @access Private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+
+    if (req.body.password) {
+      user.password = req.body.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+    });
+  } else {
+    res.status(404);
+    throw new Error('User not found');
+  }
+})
+
 export {
     authUser,
     registerUser,
     logoutUser,
+    getUserProfile,
+    updateUserProfile,
 };
