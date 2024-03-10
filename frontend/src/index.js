797755fa1927c84942e6d3793930da458d368b46
Pritan Barai <pritanbarai@Pritans-Laptop.local>
2024-03-11 03:03:21 +1100

    menu

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,31 +5,34 @@ import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } from 'react-router-dom';
 import HomeScreen from './screens/HomeScreen';
+import LoginScreen from './screens/LoginScreen';
 import RegisterScreen from './screens/RegisterScreen';
 import ProfileScreen from './screens/ProfileScreen';
 import PrivateRoute from './components/PrivateRoute';
-import StaffScreen from './screens/StaffScreen';
-import Login from './screens/Login';
+import store from './store';
+import { Provider } from 'react-redux';
+import MenuScreen from './screens/MenuScreen';
 
 const router = createBrowserRouter(createRoutesFromElements(
   <Route path='/' element={<App />}>
     <Route index={true} path='/' element={<HomeScreen />} />
-    <Route path='/login' element={<Login />} />
+    <Route path='/login' element={<LoginScreen />} />
     <Route path='/register' element={<RegisterScreen />} />
-    <Route path='/staff' element={<StaffScreen/>}/>
     { /* Private routes (user has to be logged in */}
-    <Route path='' element={<PrivateRoute />}>
+    <Route paht='' element={<PrivateRoute />}>
       <Route path='/profile' element={<ProfileScreen />} />
-      {/* <Route path='/staff' element={<StaffScrren/>}/> */}
+      <Route path='/menu' element={<MenuScreen />} />
     </Route>
   </Route>
 ))
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  <Provider store={store}>
     <React.StrictMode>
       <RouterProvider router={router}/>
     </React.StrictMode>
+  </Provider>
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ frontend/src/screens/MenuScreen.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import Sidebar from '../components/Sidebar'
+
+const MenuScreen = () => {
+  return (
+    <Sidebar></Sidebar>
+  )
+}
+
+export default MenuScreen
