c245ac45ffdcfeba2fd0b2df07a51b75a903c2f5
Jeshnil Kumar <kumar_796@hotmail.com>
2024-03-26 04:48:55 +1100

    ABNN-86 Customer selection screen now fetches list of all restaurants and displays them in a dropdown menu for selection

--- frontend/src/screens/CustomerSelectScreen.jsx
+++ frontend/src/screens/CustomerSelectScreen.jsx
@@ -1,26 +1,54 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import Button from "react-bootstrap/Button";
 import Card from "react-bootstrap/Card";
 import Dropdown from "react-bootstrap/Dropdown";
 import DropdownButton from "react-bootstrap/DropdownButton";
+import { toast } from 'react-toastify'
+
 
 const CustomerSelectScreen = () => {
+  const [restaurants, setRestaurants] = useState([])
+
   // Get a list of current restaurants
+  async function getRestaurants () {
+    const response = await fetch('http://localhost:5000/tables/restaurants', {
+      method: 'GET',
+      headers: {
+        'Content-type': 'application/json',
+      },
+    })
+    const data = await response.json();
+
+    // if successfully fetched all restaurants
+    if (response.status === 200) {
+      setRestaurants(data)
+    } else if (response.status === 204) {
+      // list of restaurants returned is empty
+      setRestaurants(data) // Not sure if I need to do anything 
+    } else {
+      // Something went wrong
+      toast.error(data?.message)
+      console.log(data?.message);
+    }
+  }
 
   // Get a list of current unoccupied tables for that restaurant
 
+  useEffect(() => {
+    getRestaurants()
+  }, [])
 
   return (
     <>
-      <h2 style={{ textAlign: "center" }}>Restaurant and Table Selection</h2>
+      <br />
       <Card className="text-center">
-        <Card.Header>Please select a restaurant and a table</Card.Header>
+        <Card.Header>Restaurant and Table Selection</Card.Header>
         <Card.Body>
           <Card.Title>Restaurant Selection</Card.Title>
-          <DropdownButton id="dropdown-basic-button" title="Dropdown button">
-            <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-            <Dropdown.Item href="#/action-2">Another action</Dropdown.Item>
-            <Dropdown.Item href="#/action-3">Something else</Dropdown.Item>
+          <DropdownButton id="dropdown-basic-button" title="Select a restaurant">
+            { restaurants.map((restaurant) => 
+              <Dropdown.Item key={restaurant.name} >{restaurant.name}</Dropdown.Item>
+            )}
           </DropdownButton>
           <Card.Text>
             With supporting text below as a natural lead-in to additional
