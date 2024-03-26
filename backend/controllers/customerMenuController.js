a0a4ec865ac99f46ac745a8047c64b67f2998a1d
Raymond Thi <raythimario1@gmail.com>
2024-03-26 20:36:10 +1100

    Fixed the bug that not all of it got pushed

--- backend/controllers/customerMenuController.js
+++ backend/controllers/customerMenuController.js
@@ -6,7 +6,7 @@ import MenuItem from '../models/itemModel.js';
 // @route  GET /:restaurantId/:tableId/menu
 // @access Public
 const getCustomerMenu = asyncHandler(async (req, res) => {
-  const { restaurantId } = req.params;
+  const { restaurantId, tableId } = req.params;
   const categories = await MenuCategory.find({ restaurant : restaurantId }).sort('position');
   const populatedCategories = await MenuCategory.populate(categories, {path: 'menuItems', options: { sort: { position: 1}}});
 
@@ -18,10 +18,10 @@ const getCustomerMenu = asyncHandler(async (req, res) => {
 });
 
 // @desc  view categories
-// @route  GET /:restaurantId/:tableId/menu
+// @route  GET /:restaurantId/:tableId/menu/categories
 // @access Public
 const getCustomerCategories = asyncHandler(async (req, res) => {
-  const { restaurantId } = req.params;
+  const { restaurantId, tableId } = req.params;
   const categories = await MenuCategory.find({ restaurant : restaurantId}).sort('position');
 
   if (categories.length > 0) {
@@ -31,8 +31,53 @@ const getCustomerCategories = asyncHandler(async (req, res) => {
   }
 });
 
+// @desc  view items
+// @route  GET /:restaurantId/:tableId/menu/categories/:categoryId/items
+// @access Public
+const getCustomerMenuItems = asyncHandler(async (req, res) => {
+  const { restaurantId, tableId, categoryId } = req.params;
+  const menuItems = await MenuItem.find({ category : categoryId }).sort('position');
+
+  if (menuItems.length > 0) {
+    res.status(200).json(menuItems);
+  } else {
+    res.status(200).json(menuItems);
+  }
+})
+
+// @desc  getCategoryItem
+// @route  GET /:restaurantId/:tableId/menu/categories/:categoryId
+// @access Public
+const getCustomerMenuCategory = asyncHandler(async (req, res) => {
+  const { restaurantId, tableId, categoryId } = req.params;
+
+  const category = await MenuCategory.findOne({_id: categoryId});
+  if (!category) {
+      res.status(404);
+      throw new Error('Menu category not found');
+  }
+  res.status(200).json(category);
+});
+
+// @desc  get menu item details
+// @route  GET /:restaurantId/:tableId/menu/categories/:categoryId/items/:itemId
+// @access Public
+const getCustomerMenuItemDetails = asyncHandler(async (req, res) => {
+  const { restaurantId, tableId, categoryId, itemId } = req.params;
+
+  const item = await MenuItem.findOne({_id: itemId, category: categoryId});
+  if (!item) {
+      res.status(404);
+      throw new Error('Menu item not found');
+  }
+  res.status(200).json(item);
+});
+
 export {
   getCustomerMenu,
-  getCustomerCategories
+  getCustomerCategories,
+  getCustomerMenuItems,
+  getCustomerMenuCategory,
+  getCustomerMenuItemDetails
 }
 
