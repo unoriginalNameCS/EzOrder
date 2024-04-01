6a2cd6b8d34815afe00fb7193c44a2fbb6f56741
Raymond Thi <raythimario1@gmail.com>
2024-04-01 17:23:02 +1100

    Getting order information

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,14 +1,15 @@
 import express from 'express';
-import { getCompletedOrders, getOrders, getPendingOrders, getPreparingOrders, setOrderInProgress, setOrderPrepared, viewOrderNotes } from '../controllers/orderController.js';
+import { getCompletedOrders, getOrders, getPendingOrders, getPreparingOrders, setOrderInProgress, setOrderPrepared, viewOrderNotes, getOrder } from '../controllers/orderController.js';
 
 const router = express.Router();
 
 router.get('/:restaurantId/orders', getOrders);
 router.put('/:restaurantId/:orderId/inProgress', setOrderInProgress);
 router.put('/:restaurantId/:orderId/prepared', setOrderPrepared);
-router.get('/:restaurantId/orders/:orderId', viewOrderNotes);
+// router.get('/:restaurantId/orders/:orderId', viewOrderNotes);
 router.get('/:restaurantId/pendingOrders', getPendingOrders);
 router.get('/:restaurantId/preparingOrders', getPreparingOrders);
 router.get('/:restaurantId/completedOrders', getCompletedOrders);
+router.get('/:restaurantId/orders/:orderId', getOrder)
 
 export default router;
