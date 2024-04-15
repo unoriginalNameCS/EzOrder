e7f6a50be7d78254468ed21e4654b50bf785d13e
Shadowmime <makdylan@gmail.com>
2024-04-16 04:20:06 +1000

    implemented clear cart, orders and requests

--- backend/routes/orderRoutes.js
+++ backend/routes/orderRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getOrders, setOrderInProgress, setOrderPrepared, setOrderServed, setOrderServing } from '../controllers/orderController.js';
+import { getOrders, setOrderInProgress, setOrderPrepared, setOrderServed, setOrderServing, clearOrders } from '../controllers/orderController.js';
 
 const router = express.Router();
 
@@ -8,5 +8,6 @@ router.put('/:restaurantId/:orderId/inProgress', setOrderInProgress);
 router.put('/:restaurantId/:orderId/prepared', setOrderPrepared);
 router.put('/:restaurantId/:orderId/serving', setOrderServing);
 router.put('/:restaurantId/:orderId/served', setOrderServed);
+router.delete('/:restaurantId/clearOrders', clearOrders);
 
 export default router;
--- backend/routes/requestRoutes.js
+++ backend/routes/requestRoutes.js
@@ -1,5 +1,5 @@
 import express from 'express';
-import { getRequests, setRequestAssisting, setRequestComplete } from '../controllers/requestController.js';
+import { getRequests, setRequestAssisting, setRequestComplete, clearRequests } from '../controllers/requestController.js';
 import { protect } from '../middleware/authMiddleware.js';
 
 const router = express.Router();
@@ -7,5 +7,6 @@ const router = express.Router();
 router.get('/:restaurantId/requests', getRequests);
 router.put('/:restaurantId/:requestId/assisting', setRequestAssisting);
 router.put('/:restaurantId/:requestId/complete', setRequestComplete);
+router.delete('/:restaurantId/clearRequests', clearRequests);
 
 export default router;
